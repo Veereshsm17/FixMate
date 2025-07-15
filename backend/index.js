426f2f2b83565a2bb55b643249fbf815098c4eca
Veeresh S M <161725057+Veereshsm17@users.noreply.github.com>
2025-07-15 23:32:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 // index.js
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
